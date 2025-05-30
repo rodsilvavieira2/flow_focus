bfcd7e3d9bc16906fb72b4e035649045d4d02b24
Rodrigo Silva <rodsilvavieira2@gmail.com>
2025-05-30 16:13:12 -0400

    Update ProgressBar widget padding and text display for improved UI

--- lib/widgets/progesss_bar.dart
+++ lib/widgets/progesss_bar.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class ProgressBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var padding = MediaQuery.of(context).size.width * 0.3;
+    var padding = MediaQuery.of(context).size.width * 0.20;
 
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: padding),
@@ -12,7 +12,23 @@ class ProgressBar extends StatelessWidget {
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [Text("Tempo restando"), Text("00:00")],
+            children: [Text("Tempo restando"), Text("0%")],
+          ),
+          SizedBox(height: 16),
+          TweenAnimationBuilder<double>(
+            duration: Duration(microseconds: 300),
+            tween: Tween<double>(begin: 0, end: 0.1),
+            builder: (context, value, child) {
+              return LinearProgressIndicator(
+                value: value,
+                minHeight: 16,
+                backgroundColor: Theme.of(context).colorScheme.secondary,
+                valueColor: AlwaysStoppedAnimation<Color>(
+                  Theme.of(context).colorScheme.primary,
+                ),
+                borderRadius: BorderRadius.circular(8),
+              );
+            },
           ),
         ],
       ),
